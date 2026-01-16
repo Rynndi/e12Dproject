33245563fb611b04fe70d39d908f57cc98329dfd
Rynndi <Catherinedi2020@gmail.com>
2026-01-15 17:23:22 -0800

    no infinite jump + feet

--- Assets/playerController.cs
+++ Assets/playerController.cs
@@ -7,8 +7,11 @@ public class newScript : MonoBehaviour
     float movementX;
     float movementY;
     [SerializeField] float speed = 5.0f; 
+    Rigidbody2D rb; 
+    bool isGrounded = false;
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>(); 
         
     }
 
@@ -34,9 +37,26 @@ public class newScript : MonoBehaviour
     //moves according to tick assigned in serialized field 
     void FixedUpdate() { 
         //because global, can take from any function 
-        float movementDistanceX = movementX * speed * Time.deltaTime; 
-        float movementDistanceY = movementY * speed * Time.deltaTime; 
-        transform.position = new Vector2(transform.position.x + movementDistanceX, transform.position.y + movementDistanceY);
+        // float movementDistanceX = movementX * speed * Time.deltaTime; 
+        // float movementDistanceY = movementY * speed * Time.deltaTime; 
+        // transform.position = new Vector2(transform.position.x + movementDistanceX, transform.position.y + movementDistanceY);
+        // rb.linearVelocity = new Vector2(movementX * speed, movementY * speed); 
+        rb.linearVelocity = new Vector2(movementX * speed, rb.linearVelocity.y);
+        if (movementY > 0 && isGrounded)
+        { 
+            rb.AddForce(new Vector2(0,100)); 
+        }
     
+    }
+    private void OnCollisionEnter2D(Collision2D collision) { 
+        if (collision.gameObject.CompareTag("Ground")) { 
+            isGrounded = true; 
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision) { 
+        if (collision.gameObject.CompareTag("Ground")) { 
+            isGrounded = false; 
+        }
     }
 }
